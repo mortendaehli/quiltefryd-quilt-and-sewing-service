991b124339e6a97420735af25b672a0ff04da185
Morten Dæhli Aslesen <mda@kvikna.no>
2025-09-14 15:43:41 +0200

    ci: fix deplyment

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { createHead } from '@vueuse/head'
 import routes from '~pages'
 import './style.css'
 import App from './App.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
 })
 
